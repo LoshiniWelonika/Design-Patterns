061124ae708ed6f135b34302f5a4ac31f22fb5bf
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-11-20 20:37:00 +0530

    D1 - After Applying Factory Design Pattern

--- src/FactoryDesignPattern/FactoryMain.java
+++ src/FactoryDesignPattern/FactoryMain.java
@@ -2,10 +2,12 @@ package FactoryDesignPattern;
 
 import FactoryDesignPattern.com.phone.Android;
 import FactoryDesignPattern.com.phone.OS;
+import FactoryDesignPattern.com.phone.OperatingSystemFactory;
 
 public class FactoryMain {
     public static void main(String[] args) {
-        OS obj = new Android();
+        OperatingSystemFactory osf = new OperatingSystemFactory();
+        OS obj = osf.getInstance("lkjdsd");
         obj.spec();
-    } 
+    }
 }
