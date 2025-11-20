2a27414a3cb95285d128f8f8f8583c93c0c4daf8
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-11-20 22:01:28 +0530

    D2 - Before Applying Builder Class

---
+++ src/BuilderPattern/com/phone/Shop.java
@@ -0,0 +1,8 @@
+package BuilderPattern.com.phone;
+
+public class Shop {
+    public static void main(String[] args) {
+        Phone p = new Phone("Android", 2, "Samsung", 5.5, 3100);
+        System.out.println(p);
+    }
+}
