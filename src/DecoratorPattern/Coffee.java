79e5b3297fd407d815738d4d7e6470e11dc99037
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-12-01 10:58:49 +0530

    Decorator Design Pattern

---
+++ src/DecoratorPattern/Coffee.java
@@ -0,0 +1,6 @@
+package DecoratorPattern;
+
+public interface Coffee {
+    double getCost();
+    String getDescription();
+}
---
+++ src/DecoratorPattern/Espresso.java
@@ -0,0 +1,13 @@
+package DecoratorPattern;
+
+public class Espresso implements Coffee{
+    @Override
+    public double getCost() {
+        return 10.0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Espresso";
+    }
+}
